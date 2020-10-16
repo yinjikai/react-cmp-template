dada7bd587c54e4a667e2aed0bbfd35ad84ec06b
尹吉凯 <1259559060@qq.com>
2020-10-16 14:51:58 +0800

    feat: 添加ts支持

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# react-cmp-template
-react组件调试模板项目
+### react 项目模板
+
+### 支持情况
+
+- ts,tsx,jsx
+- 使用 speed-measure-plugin 统计编译时间
+
+### TODO
+
+- [ ] 生产环境打包优化
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,54 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-module.exports = {
-    entry: "./src/index.js",
-    output: {
-        filename: "[name].bundle.js",
-        path: path.resolve(__dirname, "dist")
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(css|scss)$/,
-                use: ["style-loader", "css-loader", "sass-loader"]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)/,
-                use: ["file-loader"]
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader"
-                    }
-                ]
-            },
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const smp = new SpeedMeasurePlugin()
+module.exports = smp.wrap({
+  entry: './src/index.tsx',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
 
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            }
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-            filename: "./index.html"
-        })
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(tsx?|jsx?)$/,
+        // 默认会调用 @babel/core
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
     ],
-    devServer: {
-        contentBase: "./dist"
-    }
-};
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
+  devServer: {
+    contentBase: './dist',
+  },
+})
